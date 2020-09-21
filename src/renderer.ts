465191addad18a1403a655cabbefe7404bdb7cc1
Joseph Lloyd <joe.lloyd.22.24@gmail.com>
2020-09-21 21:52:22 +0200

    fixed the bug breaking the scene order

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,5 +1,3 @@
-import * as Phaser from "phaser";
-import config, { Splash, FlappyGame, GameOver } from "@joe-lloyd/flappy-bird-phaser/lib";
+import initGame from "@joe-lloyd/flappy-bird-phaser/lib";
 
-
-const game = new Phaser.Game(config);
+initGame()
